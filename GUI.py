e236402face6874658a0ba342c020bec3065ad3d
emilmagdy <dremilmagdy@gmail.com>
2025-05-24 03:08:21 +0300

    D

--- GUI.py
+++ GUI.py
@@ -4,10 +4,10 @@ from game_logic import word_list
 
 
 # variables
-green = "#4bf80f"
+
 font = ("comic sense", 60)
 letters =[]
-word = "GRADE"
+word = random.choice(word_list).upper()
 btn_index = 0
 count = 0
 guess =""
@@ -18,22 +18,9 @@ winner = False
 
 root = tk.Tk()
 root.title("Wordle")
-root.geometry("600x900")
-
-
-# new game 
-def new_game():
-    global btn_index, winner, count , char_not_present
-    layout()
-    
-    root.bind("<Key>"  , key_press)
-    
-
-menu_bar = tk.Menu(root)
-my_menu = tk.Menu(menu_bar)
-my_menu.add_command(label="New Game", command = new_game)
-root.config(menu = my_menu)
-label = tk.Label(root , text= "Wordle",  font = ("arial", 30))
+root.geometry("600x10000")
+root.configure(bg= "blue")
+label = tk.Label(root , text= "WORDLE", bg= "blue", foreground= "white", font = ("sanserif", 70))
 label.pack(padx=10, pady= 10)
 frame = tk.Frame(root)
 frame.pack()
@@ -45,12 +32,17 @@ def word_check(guess):
     btn_index -= 5
     for i in range(5):
         if letters[btn_index + i].cget("text")== word[i]:
-            letters[btn_index + i].config(bg=green)
+            letters[btn_index + i].config(bg="green")
+            letters[btn_index + i].config(activebackground = "green")
             
         elif letters[btn_index + i].cget("text") in word:
-            letters[btn_index + i].config(bg="yellow")
+            letters[btn_index + i].config(bg="orange")
+            letters[btn_index + i].config(activebackground = "orange")
+        
         else :
             letters[btn_index + i].config(bg="gray")
+            letters[btn_index + i].config(activebackground = "gray")
+
             char_not_present.append(letters[btn_index + i].cget("text"))
                                     
     if guess == word:
@@ -62,28 +54,54 @@ def word_check(guess):
 # key event
 def key_press(event):
     global btn_index, count, guess , winner
-    if winner == False:
-        letter = event.char.upper()
-        if letter not in char_not_present and letter.isalpha():
-            letters[btn_index].config(text= letter)
-            guess += letter
-            btn_index += 1
-            count += 1
-            if count % 5 == 0:
-                word_check(guess)
-                guess =""
+    if winner:
+        return
+    
+    if event.keysym == "BackSpace":
+        if btn_index> 0 and  (btn_index ) % 5 > 0:
+            btn_index -= 1
+            letters[btn_index].config(text = "")
+            count -= 1
+            guess = guess[: -1]
 
-# layout 
+    letter = event.char.upper()
+    if letter not in char_not_present and letter.isalpha():
+        letters[btn_index].config(text= letter)
+        guess += letter
+        btn_index += 1
+        count += 1
+        if count % 5 == 0:
+            word_check(guess)
+            guess =""
+
+# Grid Layout for the letters entry 
 def layout():
     for row in range(6):
         for col in range(5):
-            btn = tk.Button(frame, text = "" ,width=1, font = font, bg = "white")
+            btn = tk.Button(frame,foreground="white", bg= "black", activebackground= "black", text = "" ,width=1, font = font)
             btn.grid(row = row, column = col)
             letters.append(btn)
 
-
-    
+# New Game
+def new_game():
+    global winner, btn_index, letters, count, char_not_present, word, guess
+    word = random.choice(word_list).upper()
+    print(word)
+    guess =""
+    count = 0
+    char_not_present =[]
+    letters = []
+    btn_index = 0
+    winner = False
+    label2.forget()
+    root.bind("<Key>"  , key_press)
+    layout()
 
 new_game()
+
+menu_bar = tk.Menu(root)
+my_menu = tk.Menu(menu_bar)
+my_menu.add_command(label="New Game", command = new_game)
+root.config(menu = my_menu)
 
 root.mainloop()
--- game_logic.py
+++ game_logic.py
@@ -8,10 +8,6 @@ for word in words.words():
     if len(word) == 5:
         word_list.append(word)
 
-def generate_secret_word(word_list):
-    return random.choice(word_list)
-
-secret_word = generate_secret_word(word_list)
 
 
 
